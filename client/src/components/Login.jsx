235323aced0f266927438b87a8cfd3f8ede87fb2
Vikrant Mantalwad <vmantalwad@gmail.com>
2025-08-07 21:43:50 +0530

    Add use efffect

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,6 +4,8 @@ import { AppContext } from "../context/AppContext";
 import { motion } from "framer-motion";
 import axios from "axios";
 import { toast } from "react-toastify";
+import React, { useEffect, useState } from "react";
+
 
 const Login = () => {
   const [state, setState] = useState("Login");
